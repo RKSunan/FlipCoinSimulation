469e9ad7cf5e01070c0e36d0620b8161e696763a
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 18:01:47 +0530

    FlippingCoinMultipleTimes

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 #echo "This problem displays the winner Heads or Tails"
+head=0
+tail=0
+for i in $(seq 1 20)
+do
 flip=$((RANDOM%2))
 if [ $flip == 1 ]
 then
 	echo "Head Wins"
+	((head++))
 else
 	echo "Tail Wins"
+	((tail++))
 fi
+done
+echo "Head has won $head times"
+echo "Tail has won $tail times"
