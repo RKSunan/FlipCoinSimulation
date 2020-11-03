b4c1ea14fbae79a2e6cda69b1c1de2f2e17d618c
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 18:28:36 +0530

    UC3-FlippingCoinWithaWon

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,6 +5,8 @@ tail=0
 for i in $(seq 1 100)
 do
 flip=$((RANDOM%2))
+if [[ $head -lt 21 && $tail -lt 21 ]]
+then
 if [ $flip == 1 ]
 then
 #	echo "Head Wins"
@@ -12,7 +14,20 @@ then
 else
 #	echo "Tail Wins"
 	((tail++))
+
+fi
 fi
 done
 echo "Head has won $head times"
 echo "Tail has won $tail times"
+if [ $head == $tail ]
+then
+	echo "There is a tie"
+elif [ $head -gt $tail ]
+then
+	n=$(($head - $tail))  
+	echo "Head wins $n more"
+else
+	n=$(($tail - $head))
+	echo "Tail wins $n more"
+fi
