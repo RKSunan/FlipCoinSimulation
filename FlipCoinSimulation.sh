0d78b8f069cb8853aaf40a4115ceff60e4711875
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 17:54:03 +0530

    FlippingCoin

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,9 @@
-echo "This problem displays the winner Heads or Tails"
-
+#! /bin/bash
+#echo "This problem displays the winner Heads or Tails"
+flip=$((RANDOM%2))
+if [ $flip == 1 ]
+then
+	echo "Head Wins"
+else
+	echo "Tail Wins"
+fi
